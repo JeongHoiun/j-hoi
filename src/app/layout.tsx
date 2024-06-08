4ef475e3a72a2e7af1b52122377b300ce6a4b394
hoiunjeong <un4255@gmail.com>
2024-06-08 17:53:37 +0900

    홈에 썸네일 넣기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,8 @@ export default function RootLayout({
     <html lang="en">
       <body
         style={{
+          width: '100vw',
+          height: '100vh',
           margin: 0,
           padding: 0,
           fontFamily: 'sans-serif'
@@ -32,16 +34,15 @@ export default function RootLayout({
             gap={32}
             borderBottom={`1px solid ${BORDER_COLOR}`}
           >
-            <Button>
-              <Link sx={{ textDecoration: 'None' }} href="/">
-                Home
-              </Link>
-            </Button>
-            <Button>
-              <Link sx={{ textDecoration: 'None' }} href="/blog">
-                Blog
-              </Link>
-            </Button>
+            <Link sx={{ textDecoration: 'None' }} href="/">
+              Home
+            </Link>
+            <Link sx={{ textDecoration: 'None' }} href="/blog">
+              Blog
+            </Link>
+            <Link sx={{ textDecoration: 'None' }} href="/profile">
+              Profile
+            </Link>
           </Box>
           {children}
         </Box>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
-import { Typography } from '@mui/material';
+import { Box } from '@mui/material';
+import Image from 'next/image';
 
 export default function Home() {
-  return <Typography variant="h1">HOI Hello World!!!!</Typography>;
+  return (
+    <Box textAlign="center" marginTop="16px" width="100%">
+      <Image src="/thumbnail.png" alt="Thumbnail" width={820} height={820} />
+    </Box>
+  );
 }
